cdfcf3e6af2d7ba7dcb2f723aaf56797b495bb72
Jordi Montoya <jmontoya@stacks-cis.ct>
2019-02-06 16:59:56 +0100

    added chrome driver in config file

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 PATH_FIREFOX_DRIVER = r"driver\geckodriver-v0.24.0-win64\geckodriver.exe"
+PATH_CHROME_DRIVER = r"driver\chromedriver_win32\chromedriver.exe"
+BROWSER = "CHROME"
 BASE_URL = "https://twitter.com/login"
 DELAY = 2  # time to wait on each page load before reading the page
 USERNAME = "your_username"
